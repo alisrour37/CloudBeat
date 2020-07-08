24507077f397cd0c2fe2636ebb8ebc87e0961603
alisrour37 <63813447+alisrour37@users.noreply.github.com>
2020-07-08 18:23:39 +0300

    Unified React and .net core folder

---
+++ CloudBeatAsp/CloudBeatAsp/ClientApp/src/index.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+import PatientsTable from './PatientsTable';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <BrowserRouter basename={baseUrl}>
+        <PatientsTable />
+  </BrowserRouter>,
+  rootElement);
+
+registerServiceWorker();
+
