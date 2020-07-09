80f809d2c171df6c235e270bd63c8d4a14f56a46
alisrour37 <63813447+alisrour37@users.noreply.github.com>
2020-07-09 19:00:45 +0300

    Controller Initialization

---
+++ CloudBeatAsp/CloudBeatAsp/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudBeatAsp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudBeatAsp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        [Route("Events/{patientId}")]
+        public IActionResult GetValues()
+        {
+                var events = _context.Events.ToList();
+                return Ok(events);
+        }
+    }
+}
---
+++ CloudBeatAsp/CloudBeatAsp/Controllers/PatientController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CloudBeatAsp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudBeatAsp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public PatientController(DataContext context)
+        {
+                        _context = context;
+        }
+        [HttpGet]
+        [Route("Patients")]
+        public IActionResult GetPatient(){
+            var patients = _context.Patients.ToList();
+            return Ok(patients);
+        }
+
+    }
+}
+
+
